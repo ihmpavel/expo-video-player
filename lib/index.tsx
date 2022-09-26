769f7f9de69f8c4a0861b5a763052b2f8e7e65da
luca.pezzolla <me@lucapezzolla.com>
2022-09-26 22:24:03 +0200

    fix: prevent accidental pressing of buttons in header when the overlay is not visible

--- lib/index.tsx
+++ lib/index.tsx
@@ -243,6 +243,7 @@ const VideoPlayer = (tempProps: Props) => {
       />
 
       <Animated.View
+        pointerEvents={controlsState === ControlStates.Visible ? 'auto' : 'none'}
         style={[
           styles.topInfoWrapper,
           {
